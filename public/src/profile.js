121fc71e60a6da05aaaaef7ff7cf7f3c209154aa
PaulaB03 <balan.paula1@gmail.com>
2023-06-04 15:24:20 +0300

    Impletemnt edit information

--- public/src/profile.js
+++ public/src/profile.js
@@ -1,3 +1,7 @@
+const iconClose = document.querySelector('.icon-close');
+const infoTab = document.querySelector('.login-tab');
+const modifyBtn = document.querySelector('.modify');
+
 username = document.getElementById("name");
 phone = document.getElementById("phone");
 gender = document.getElementById("gender");
@@ -6,17 +10,19 @@ illness = document.getElementById("illness");
 alergy = document.getElementById("alergy");
 appointments = document.getElementById('appointments');
 
-// Fetch the user information from the server
-fetch('/profile/user')
-  .then(response => response.json())
-  .then(user => {
-    renderProfile(user);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+function fetchUserProfile() {
+  // Fetch the user information from the server
+  fetch('/profile/user')
+    .then(response => response.json())
+    .then(user => {
+      renderProfile(user);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+}
 
-// Fatch the appointments
+// Fetch the appointments
 fetch('http://localhost:5000/programari')
 .then(function (response) {
     response.json().then(function (programari) {
@@ -25,20 +31,15 @@ fetch('http://localhost:5000/programari')
     })
 })
 
+
 // Function to render the user information
 function renderProfile(user) {
-    const userName = document.createElement('p').classList.toggle('info').textContent = `${user.nume}`;
-    username.append(userName);
-    const userPhone = document.createElement('p').classList.toggle('info').textContent = `${user.nr_telefon}`;
-    phone.append(userPhone);
-    const userGender = document.createElement('p').classList.toggle('info').textContent = `${user.sex}`;
-    gender.append(userGender);
-    const userBlood = document.createElement('p').classList.toggle('info').textContent = `${user.grupa_sanguina}`;
-    blood.append(userBlood);
-    const userIllness = document.createElement('p').classList.toggle('info').textContent = `${user.afectiuni}`;
-    illness.append(userIllness);
-    const userAlergy = document.createElement('p').classList.toggle('info').textContent = `${user.alergii}`;
-    alergy.append(userAlergy);
+  document.getElementById('nume').textContent = user.nume;
+  document.getElementById('telefon').textContent = user.nr_telefon;
+  document.getElementById('sex').textContent = user.sex || 'N/A';
+  document.getElementById('sange').textContent = user.grupa_sanguina || 'N/A';
+  document.getElementById('afectiuni').textContent = user.afectiuni || 'N/A';
+  document.getElementById('alergii').textContent = user.alergii || 'N/A';
 }
 
 
@@ -68,8 +69,12 @@ function appendAppointment(appointment) {
       month: '2-digit',
       year: 'numeric'
     });
-
     date.innerText = formattedDate;
+
+    // Create time obj
+    let time = document.createElement('p');
+    time.classList.toggle('time');
+    time.innerText = appointment['ora'].substring(0, 5);
 
     // Create appointment obj
     let doc = document.createElement('div');
@@ -80,5 +85,67 @@ function appendAppointment(appointment) {
     doc.appendChild(name);
     doc.appendChild(room);
     doc.appendChild(date);
+    doc.appendChild(time);
     appointments.appendChild(doc);
 }
+
+
+// Eveniment buton cancel 
+iconClose.addEventListener('click', () => {
+  infoTab.style.display = 'none'; 
+}) 
+
+// Eveniment buton modificare informatii
+modifyBtn.addEventListener('click', () => {
+  infoTab.style.display = 'block';
+
+  // Make an AJAX request to retrieve user information
+  fetch('http://localhost:5000/profile/user')
+    .then(response => response.json())
+    .then(user => {
+      // Prefill the input fields with user information
+      document.getElementById('phoneForm').value = user.nr_telefon;
+      document.getElementById('illnessForm').value = user.afectiuni;
+      document.getElementById('alergyForm').value = user.alergii;
+    })
+    .catch(error => {
+      console.error('Error retrieving user information:', error);
+    });
+})
+
+// Event listener for form submission
+document.getElementById("submitInfo").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+
+  // Get the form inputs
+  const phone = document.getElementById("phoneForm").value;
+  const illness = document.getElementById("illnessForm").value;
+  const allergy = document.getElementById("alergyForm").value;
+
+  // Create a data object with the updated user information
+  const data = {
+    phone: phone,
+    illness: illness,
+    allergy: allergy
+  };
+
+  // Send a POST request to the server with the updated user information
+  fetch('/modify', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(result => {
+    infoTab.style.display = 'none';
+    fetchUserProfile();
+    console.log(result);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
+});
+
+fetchUserProfile();
